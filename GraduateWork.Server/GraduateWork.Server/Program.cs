836a12d688d3fda8d59117b1bd4645233ab3d79e
Vova2999 <Vova2999@gmail.com>
2017-03-11 14:02:14 +0400

    Server: Создание сервера

---
+++ GraduateWork.Server/GraduateWork.Server/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace GraduateWork.Server {
+	public static class Program {
+		[DllImport("kernel32.dll")]
+		private static extern IntPtr GetConsoleWindow();
+
+		[DllImport("user32.dll")]
+		private static extern bool ShowWindow(IntPtr hWnd, int mCmdShow);
+
+		private const int swHide = 0;
+		private const int swShow = 5;
+
+		public static void Main(string[] args) {
+			var handle = GetConsoleWindow();
+
+			ShowWindow(handle, swHide);
+			Console.ReadKey();
+		}
+	}
+}
