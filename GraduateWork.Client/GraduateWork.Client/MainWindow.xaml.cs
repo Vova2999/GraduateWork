a5c21ca26e75b21cdbac6126a667bb8feda73487
Vova2999 <Vova2999@gmail.com>
2017-03-12 13:12:56 +0400

    Client: Сделал набросок таблиц

--- GraduateWork.Client/GraduateWork.Client/MainWindow.xaml.cs
+++ GraduateWork.Client/GraduateWork.Client/MainWindow.xaml.cs
@@ -1,7 +1,19 @@
-﻿namespace GraduateWork.Client {
+﻿using System.Windows;
+
+namespace GraduateWork.Client {
 	public partial class MainWindow {
 		public MainWindow() {
 			InitializeComponent();
+		}
+		
+		private void ButtonUpdateTableGroups_OnClick(object sender, RoutedEventArgs e) {
+			throw new System.NotImplementedException();
+		}
+		private void ButtonUpdateTableDisciplines_OnClick(object sender, RoutedEventArgs e) {
+			throw new System.NotImplementedException();
+		}
+		private void ButtonUpdateTableStudents_OnClick(object sender, RoutedEventArgs e) {
+			throw new System.NotImplementedException();
 		}
 	}
 }
