7bd80ff38f195fa14f97208d1db939834897a5d8
Vova2999 <Vova2999@gmail.com>
2017-05-07 22:42:16 +0400

    Server: Доделал создание отчета диплома

--- GraduateWork.Server/GraduateWork.Server.Reports/Creators/DiplomaCreator.cs
+++ GraduateWork.Server/GraduateWork.Server.Reports/Creators/DiplomaCreator.cs
@@ -1,4 +1,5 @@
-﻿using GraduateWork.Common.Database.Readers;
+﻿using System.Globalization;
+using GraduateWork.Common.Database.Readers;
 using GraduateWork.Common.Tables.Proxies.Extendeds;
 using TemplateEngine.Docx;
 
@@ -15,10 +16,21 @@ namespace GraduateWork.Server.Reports.Creators {
 		}
 
 		protected override Content CreateContent(StudentExtendedProxy student, DisciplineExtendedProxy[] disciplines, GroupExtendedProxy group) {
+			var registrationDate = student.RegistrationDate.ToString("dd MMMM yyyy", CultureInfo.CreateSpecificCulture("ru-RU")).Split();
+
 			return new Content(
+				new FieldContent("RegistrationNumber", student.RegistrationNumber),
+				new FieldContent("ProtectionDate", student.ProtectionDate.ToString("dd MMMM yyyy", CultureInfo.CreateSpecificCulture("ru-RU"))),
+				new FieldContent("RegistrationNumber", student.RegistrationNumber),
 				new FieldContent("FirstName", student.FirstName),
 				new FieldContent("SecondName", student.SecondName),
-				new FieldContent("ThirdName", student.ThirdName));
+				new FieldContent("ThirdName", student.ThirdName),
+				new FieldContent("SpecialtyNumber", group.SpecialtyNumber.ToString()),
+				new FieldContent("FacultyName", group.FacultyName),
+				new FieldContent("ProtocolNumber", student.ProtocolNumber),
+				new FieldContent("RegistrationDateDay", registrationDate[0]),
+				new FieldContent("RegistrationDateMonth", registrationDate[1]),
+				new FieldContent("RegistrationDateYear", registrationDate[2]));
 		}
 	}
 }
